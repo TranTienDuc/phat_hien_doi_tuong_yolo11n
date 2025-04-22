12e081f90e6754d35a8d7bffd7ccb69e88ee5896
TranTienDuc <trantienduc@gmail.com>
2025-04-22 14:09:41 +0700

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -125,6 +125,32 @@ class Inference:
         elif self.source == "webcam":
             from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode, RTCConfiguration
             import av            
+
+            RTC_CONFIGURATION = RTCConfiguration(
+                {
+                  "iceServers": [{
+                    "urls": ["turn:a.relay.metered.ca:443"],
+                    "username": "1c8984b57b00688cab1ff9ce",
+                    "credential": "NVNrmuBuC6u5mVM/",
+                  }]
+                }
+            )
+            
+            class VideoProcessor:
+                def recv(self, frame):
+                    img = frame.to_ndarray(format="bgr24")
+                    return av.VideoFrame.from_ndarray(img, format="bgr24")
+            
+            webrtc_ctx = webrtc_streamer(
+                key="WYH",
+                mode=WebRtcMode.SENDRECV,
+                rtc_configuration=RTC_CONFIGURATION,
+                media_stream_constraints={"video": True, "audio": False},
+                video_processor_factory=VideoProcessor,
+                async_processing=True,
+            ) 
+            
+            
             self.vid_file_name = 0  # Use webcam index 0
 
     def configure(self):
